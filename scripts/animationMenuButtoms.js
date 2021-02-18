b665d03c060887ddb111cf2a7d5915c6d3e178ab
Diego Garcia <diegogar92@gmail.com>
2021-02-18 15:54:23 -0500

    Se agrego animacion para todos los botones

--- scripts/animationMenuButtoms.js
+++ scripts/animationMenuButtoms.js
@@ -1,10 +1,21 @@
+var boton;
+var sombra;
+var botones;
+var sombras;
 function comenzar() {
 
-var boton=document.querySelector(".menu_items");
-var sombra=document.querySelector(".menu_items--sombra");
+    boton=document.querySelector(".menu_items");
+    botones=document.querySelectorAll(".menu_items");
+    sombra=document.querySelector(".menu_items--sombra");
+    sombras=document.querySelectorAll(".menu_items--sombra");
+    
+/* boton.addEventListener("mousemove",eventoPrincipal,false);
+boton.addEventListener("mouseout",eventoNormalizar,false); */
 
-boton.addEventListener("mousemove",eventoPrincipal,false);
-boton.addEventListener("mouseout",eventoNormalizar,false);
+    botones.forEach(function(item){
+        item.addEventListener("mousemove",eventoPrincipal,false);
+        item.addEventListener("mouseout",eventoNormalizar,false);
+    });
 
 function eventoPrincipal(e){
     //console.log(e.offsetX+" - "+e.offsetY);
@@ -14,21 +25,31 @@ function eventoPrincipal(e){
     var valueY=e.offsetY;
     var ResultX=(-(18*100*valueY)/(50*maxHeight))+18;
     var ResultY=((18*100*valueX)/(50*maxWidth))-18;
+    let item=e.srcElement;
+    let itemParent=e.srcElement.parentElement;
     //console.log(ResultX+","+ResultY);
-    boton.setAttribute("style", "transform: translate3d(0px, 0px, 40px) rotateX("+ResultX+"deg) rotateY("+ResultY+"deg);");
-    var porcentaje=(valueX*100)/maxWidth;
-    sombra.setAttribute("style",`background: linear-gradient(140deg, rgba(159,184,173,0) 0%, rgba(0,0,38,1) ${porcentaje}%, rgba(44,181,232,0) 100%);`);
+    if(true){
+        itemParent.setAttribute("style", "transform: translate3d(0px, 0px, 40px) rotateX("+ResultX+"deg) rotateY("+ResultY+"deg);");
+    }
+    let porcentaje=(valueX*100)/maxWidth;
+    if(true){
+        item.setAttribute("style",`background: linear-gradient(140deg, rgba(159,184,173,0) 0%, rgba(0,0,38,1) ${porcentaje}%, rgba(44,181,232,0) 100%);`);
+    }
 }
 
-function eventoNormalizar(){
-    boton.setAttribute("style", "transform: translate3d(0px, 0px, 0px) rotateX(0deg) rotateY(0deg);");
-    sombra.setAttribute("style",`background: linear-gradient(140deg, rgba(159,184,173,0) 0%, rgba(0,0,38,1) 50%, rgba(44,181,232,0) 100%);`);    
+function eventoNormalizar(e){
+    let item=e.srcElement;
+    let itemParent=e.srcElement.parentElement;
+    itemParent.setAttribute("style", "transform: translate3d(0px, 0px, 0px) rotateX(0deg) rotateY(0deg);");
+    item.setAttribute("style",`background: linear-gradient(140deg, rgba(159,184,173,0) 0%, rgba(0,0,38,1) 50%, rgba(44,181,232,0) 100%);`);    
 }
 }
 
 function cargar(){
     setTimeout(function (){
-        let date=new comenzar;
+        var misObjetos=new comenzar;
+        console.log(boton);
+
     },4000);
 }
 
