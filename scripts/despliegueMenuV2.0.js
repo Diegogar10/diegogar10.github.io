22939903e884157a2da661856a1d923e6f529773
Diego Garcia <diegogar92@gmail.com>
2021-03-17 13:48:26 -0500

    menu finalizado: cambios en javaScrip ahora nativo para efecto boton que despliega el menu con cambio de clases desde css

--- scripts/despliegueMenuV2.0.js
+++ scripts/despliegueMenuV2.0.js
@@ -1,10 +1,20 @@
 function load(){
-    let menu=document.querySelector(".nav");
+    let botonmenu=document.querySelector(".nav");
 
-    menu.addEventListener("click",efectoMenu,false);
+    botonmenu.addEventListener("click",efectoMenu,false);
 }
 
-function efectoMenu(){
+function efectoMenu(e){
+
+    let menu=document.querySelector(".menu");
+    let mainslogan=document.querySelector(".main-slogan");
+    let boton=e.target;
+
+    menu.classList.toggle("menu-desplegado1");
+    mainslogan.classList.toggle("main-slogan--retraicted")
+
+    if(boton.value =='=')boton.value ='x';
+    else boton.value = '=';
     
 }
 window.onload=load;
