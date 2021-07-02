f0270c8d3675175fe5f164f2c23e9b87ac0f3c43
Diego Garcia <diegogar92@gmail.com>
2021-07-02 15:56:55 -0500

    Actualizacion pacial prepros, reorganizacion y borrado de archivos no necesarios

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,10 +2,10 @@ import inicio from './scripts/header-menu-animacion.js';
 import AnimacionBotones from './scripts/header-menu-botones.js';
 import FondoRandomBotones from './scripts/main-services-botones.js';
 
-
+//---------------------
 inicio();
-
+//---------------------
 const animacion = new AnimacionBotones();
-
+//---------------------
 const fondoRandom = new FondoRandomBotones();
 fondoRandom.setBackgroundImage();
--- src/assets/reproduccionAuto.js
+++ src/assets/reproduccionAuto.js
@@ -7,10 +7,6 @@ const player = new MediaPlayer({el:video, plugins:[
     new AutoPlay()
 ]});
 
-/*   if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('./sw.js').catch(error=>{
-        console.log(error.message);
-    });
-}  */
+
  
 
