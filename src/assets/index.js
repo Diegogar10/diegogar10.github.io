2f66e97c4300e08548d354acda0f5deed60e7be8
Diego Garcia <diegogar92@gmail.com>
2021-09-01 16:11:29 -0500

    primeros cambio, implementacion de webpack y babel, junto con plugins de html y css

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,11 @@
+import '../styles/generales.css';
+import reproducion from './reproduccionAuto.js';
 import inicio from './scripts/header-menu-animacion.js';
 import AnimarBotonesMenu from './scripts/header-menu-botones.js';
 import FondoRandomBotones from './scripts/main-services-botones.js';
 import AnimarBotonesMain from './scripts/main-botones-animacion.js';
-
+//---------------------
+reproducion();
 //---------------------
 inicio();
 //---------------------
--- src/assets/reproduccionAuto.js
+++ src/assets/reproduccionAuto.js
@@ -1,12 +1,16 @@
 import MediaPlayer from './lib/MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
 
-let video = document.querySelector(".mainContainer__fondo");
 
-const player = new MediaPlayer({el:video, plugins:[
-    new AutoPlay()
-]});
+const reproduccion = () => {
+    
+    let video = document.querySelector(".mainContainer__fondo");
+    
+    const player = new MediaPlayer({el:video, plugins:[
+        new AutoPlay()
+    ]});
+}
 
-
+export default reproduccion;
  
 
